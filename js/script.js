8adf93f2c6e467d98cd0bb3b109dce8987f439d9
marypaduano <maria.paduano@gmail.com>
2023-02-14 15:35:15 +0100

    add literal object team

---
+++ js/script.js
@@ -0,0 +1,34 @@
+console.log('team')
+
+const team = [
+    {
+      name: "Wayne Barnett",
+      role: "Founder & CEO",
+      photo: "img/wayne-barnett-founder-ceo.jpg",
+    },
+    {
+      name: "Angela Caroll",
+      role: "Chief Editor",
+      photo: "img/angela-caroll-chief-editor.jpg",
+    },
+    {
+      name: "Walter Gordon",
+      role: "Office Manager",
+      photo: "img/walter-gordon-office-manager.jpg",
+    },
+    {
+      name: "Angela Lopez",
+      role: "Social Media Manager",
+      photo: "img/angela-lopez-social-media-manager.jpg",
+    },
+    {
+      name: "Scott Estrada",
+      role: "Developer",
+      photo: "img/scott-estrada-developer.jpg",
+    },
+    {
+      name: "Barbara Ramos",
+      role: "Graphic Designer",
+      photo: "img/barbara-ramos-graphic-designer.jpg",
+    }
+  ]
